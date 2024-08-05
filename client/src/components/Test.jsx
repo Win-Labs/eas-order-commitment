f5a57e69cffa64eb18ac569cd9c0d2d648f4270d
gylman <gylmanbm@gmail.com>
2024-08-05 09:56:49 +0900

    converting clients to providers and signers

--- client/src/components/Test.jsx
+++ client/src/components/Test.jsx
@@ -1,19 +1,76 @@
-import React, { useEffect, useState } from "react";
-import { useReadContract, useWriteContract, useAccount } from "wagmi";
+import React, { useContext, useEffect, useState } from "react";
+import {
+  useReadContract,
+  useWriteContract,
+  useAccount,
+  useClient,
+  useConnectorClient,
+} from "wagmi";
 import { hhContractAbi, hhContractAddress } from "../config.js";
 import classes from "./Test.module.css";
 import { useWatchContractEvent } from "wagmi";
-import { SchemaRegistry } from "@ethereum-attestation-service/eas-sdk";
+import { WagmiContext } from "wagmi";
+
+import {
+  EAS,
+  Offchain,
+  SchemaEncoder,
+  SchemaRegistry,
+} from "@ethereum-attestation-service/eas-sdk";
 import { ethers } from "ethers";
+import {
+  clientToProvider,
+  useEthersProvider,
+} from "../hooks/clientToProvider.js";
+import { clientToSigner } from "../hooks/connectorClientToSigner.js";
 
 const Test = () => {
   const [output, setOutput] = useState("");
   const { address } = useAccount();
+  const publicClient = useClient();
+  const walletClient = useConnectorClient();
+  console.log("this is the useClient()", publicClient);
+  console.log("this is the useConnectorClient()", walletClient);
 
   const connectWallet = () => {
     console.log("called connectWallet");
     setOutput(`called connectWallet: ${address}`);
   };
+
+  const EASContractAddress = "0xC2679fBD37d54388Ce493F1DB75320D236e1815e"; // Sepolia v0.26
+
+  // Initialize the sdk with the address of the EAS Schema contract address
+  const eas = new EAS(EASContractAddress);
+
+  // Gets a default provider (in production use something else like infura/alchemy)
+  const provider = useEthersProvider({ chainId: 11155420 });
+
+  console.log("this is the provider", provider);
+
+  // Connects an ethers style provider/signingProvider to perform read/write functions.
+  // MUST be a signer to do write operations!
+  eas.connect(provider);
+
+  const schemaRegistryContractAddress =
+    "0x0a7E2Ff54e76B8E6659aedc9103FB21c038050D0"; // Sepolia 0.26
+  const schemaRegistry = new SchemaRegistry(schemaRegistryContractAddress);
+
+  console.log(schemaRegistry);
+  schemaRegistry.connect(provider);
+
+  const schemaUID =
+    "0x3ee426c18d2dce68267f5fd2050b82687b51608819d7c2503f6f0ff0ac5ca1c8";
+
+  useEffect(() => {
+    const logSchemaRecord = async () => {
+      console.log(schemaRegistry);
+      const schemaRecord = await schemaRegistry.getSchema({ uid: schemaUID });
+
+      console.log(schemaRecord);
+    };
+
+    logSchemaRecord();
+  }, []);
 
   return (
     <div
--- client/src/config.js
+++ client/src/config.js
@@ -1,23 +1,10 @@
 import { http, createConfig, injected } from "@wagmi/core";
-import { defineChain } from "viem";
-
-export const localhost = /*#__PURE__*/ defineChain({
-  id: 31337,
-  name: "Localhost",
-  nativeCurrency: {
-    decimals: 18,
-    name: "Ether",
-    symbol: "ETH",
-  },
-  rpcUrls: {
-    default: { http: ["http://127.0.0.1:8545"] },
-  },
-});
+import { optimismSepolia } from "viem/chains";
 
 export const config = createConfig({
-  chains: [localhost],
+  chains: [optimismSepolia],
   transports: {
-    [localhost.id]: http(),
+    [optimismSepolia.id]: http(),
   },
   connectors: [injected()],
 });
